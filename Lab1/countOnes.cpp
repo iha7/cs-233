a7e1bcbee7b614af522940130f39fc3d88315f0e
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-08-30 19:10:22 -0500

    done i think

--- Lab1/countOnes.cpp
+++ Lab1/countOnes.cpp
@@ -4,17 +4,13 @@
  */
 
 unsigned countOnes(unsigned input) {
-	// TODO: write your code here
-	unsigned left_shift = input & 0x55555555;
-	unsigned right_shift = input & 0xaaaaaaaa;
+	// // TODO: write your code here
 
-	unsigned output = left_shift + (right_shift >> 1);
+unsigned output = (input & 0x55555555) + ((input & 0xaaaaaaaa) >> 1);
 
-	output = (output & 0x33333333) + ((output & 0xcccccccc) >> 2);
-	output = (output & 0x0f0f0f0f) + ((output & 0xf0f0f0f0) >> 4);
-	output = (output & 0x00ff00ff) + ((output & 0xff00ff00) >> 8);
-	output = (output & 0x0000ffff) + ((output & 0xffff0000) >> 16);
-	
-
-	return output;
+output = (output & 0x33333333) + ((output & 0xcccccccc) >> 2);
+output = (output & 0x0f0f0f0f) + ((output & 0xf0f0f0f0) >> 4);
+output = (output & 0x00ff00ff) + ((output & 0xff00ff00) >> 8);
+output = (output & 0x0000ffff) + ((output & 0xffff0000) >> 16);
+return output;
 }
