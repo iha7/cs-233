e3b5e686b6d1cf34b771114379ece01637522eb3
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-09-02 18:36:57 -0500

    lab part 1

--- Lab2/logicunit.v
+++ Lab2/logicunit.v
@@ -1,7 +1,33 @@
 // 00 -> AND, 01 -> OR, 10 -> NOR, 11 -> XOR
-module logicunit(out, A, B, control);
-    output      out;
-    input       A, B;
-    input [1:0] control;
+module mux4v(out, A, B, C, D, control);
+  output      out;
+  input       A, B, C, D;
+  input [1:0] control;
 
-endmodule // logicunit
+  wire  wA, wB, wC, wD;
+
+  assign wA = A & (control == 2'b00);
+  assign wB = B & (control == 2'b01);
+  assign wC = C & (control == 2'b10);
+  assign wD = D & (control == 2'b11);
+
+  or  o1(out, wA, wB, wC, wD);
+
+endmodule // mux4v
+
+// Design a logicunit that implements the specified truth table
+//00 -> AND, 01 -> OR, 10 -> NOR, 11 -> XOR
+
+module logicunit(out, A, B, control);
+  output      out;
+  input       A, B;
+  input [1:0] control;
+  wire an, o, no, xo;
+
+  and na1(an, A, B);
+  or a1(o, A, B);
+  nor o1(no, A, B);
+  xor no1(xo, A, B);
+  mux4v mux4(out, an, o, no, xo, control);
+
+endmodule // Logic Unit to Verilog
--- Lab2/mux.v
+++ Lab2/mux.v
@@ -1,10 +1,33 @@
+// // output is A (when control == 0) or B (when control == 1)
+// module mux2(out, A, B, control);
+//     output out;
+//     input  A, B;
+//     input  control;
+//     wire   wA, wB, not_control;
+         
+//     not n1(not_control, control);
+//     and a1(wA, A, not_control);
+//     and a2(wB, B, control);
+//     or  o1(out, wA, wB);
+// endmodule // mux2
+
+// // output is A (when control == 00) or B (when control == 01) or
+// //           C (when control == 10) or D (when control == 11)
+// module mux4(out, A, B, C, D, control);
+//     output      out;
+//     input       A, B, C, D;
+//     input [1:0] control;
+
+// endmodule // mux4
+
+
 // output is A (when control == 0) or B (when control == 1)
 module mux2(out, A, B, control);
     output out;
     input  A, B;
     input  control;
     wire   wA, wB, not_control;
-         
+
     not n1(not_control, control);
     and a1(wA, A, not_control);
     and a2(wB, B, control);
@@ -17,5 +40,10 @@ module mux4(out, A, B, C, D, control);
     output      out;
     input       A, B, C, D;
     input [1:0] control;
+    wire m1, m2;
+
+    mux2 mu1(m1, A, B, control[0]);
+    mux2 mu2(m2, C, D, control[0]);
+    mux2 mu3(out, m1, m2, control[1]);
 
 endmodule // mux4
