f0e43ab359f2cb3637e5c8bae434f8bac12d0812
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-09-27 15:23:22 -0500

    part 2 i think we did it

--- Lab5/decoder.v
+++ Lab5/decoder.v
@@ -18,16 +18,69 @@
 // zero          (input) - from the ALU
 //
 
+// module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
+//                    mem_read, word_we, byte_we, byte_load, slt, lui, addm,
+//                    opcode, funct, zero);
+//     output [2:0] alu_op;
+//     output [1:0] alu_src2;
+//     output       writeenable, rd_src, except;
+//     output [1:0] control_type;
+//     output       mem_read, word_we, byte_we, byte_load, slt, lui, addm;
+//     input  [5:0] opcode, funct;
+//     input        zero;
+
+
+//     wire addi = (opcode == `OP_ADDI);
+//     wire andi = (opcode == `OP_ANDI);
+//     wire ori = (opcode == `OP_ORI);
+//     wire xori = (opcode == `OP_XORI);
+
+//     wire or1 = (opcode == `OP_OTHER0) & (funct == `OP0_OR);
+//     wire nor1 = (opcode == `OP_OTHER0) & (funct == `OP0_NOR);
+//     wire xor1 = (opcode == `OP_OTHER0) & (funct == `OP0_XOR);
+//     wire add = (opcode == `OP_OTHER0) & (funct == `OP0_ADD);
+//     wire sub = (opcode == `OP_OTHER0) & (funct == `OP0_SUB);
+//     wire andd = (opcode == `OP_OTHER0) & (funct == `OP0_AND);
+
+
+//     wire lw1 = (opcode == `OP_LW);
+//     wire lbu1 = (opcode == `OP_LBU);
+//     wire sw1 = (opcode == `OP_SW);
+//     wire sb1 = (opcode == `OP_SB);
+//     wire bne1 = (opcode == `OP_BNE);
+//     wire beq1 = (opcode == `OP_BEQ);
+//     wire j1 = (opcode == `OP_J);
+//     wire lui1 = (opcode == `OP_LUI);
+
+//     wire addm1 = (opcode == `OP_OTHER0) & (funct == `OP0_ADDM);
+//     wire slt1 = (opcode == `OP_OTHER0) & (funct == `OP0_SLT);
+//     wire jr1 = (opcode == `OP_OTHER0) & (funct == `OP0_JR);
+//     assign control_type[0] = ((bne1 & ~zero) | (beq1 & zero) | jr1);
+//     assign control_type[1] = (j1 | jr1);
+
+//     assign alu_op[0] = (xori | bne1 | beq1 | slt1 | sub | or1 | xor1 | ori);
+//     assign alu_op[1] = (add | sub | beq1 | lw1 | lbu1 | sw1 | sb1 | slt1 | nor1 | xor1 | addi | xori | bne1);
+//     assign alu_op[2] = (xor1 | andi | ori | xori | andd | or1 | nor1);
+//     assign writeenable = (andi | ori | xori | lw1 | lbu1 | lui1 | slt1 | addm1 | add | sub | andd | or1 | nor1 | xor1 | addi);
+//     assign rd_src = (lw1 | lbu1 | lui1 | addi | andi | ori | xori);
+//     assign alu_src2 = (lw1 | lbu1 | sw1 | sb1 | addi | andi | ori | xori);
+//     assign except = ~(nor1 | xor1 | addi | andi | lw1 | lbu1 | sw1 | sb1 | addm1 | add | sub | andd | or1 | ori | xori | bne1 | beq1 | j1 | jr1 | lui1 | slt1);
+
+//     endmodule // mips_decode
+
+
+
 module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
                    mem_read, word_we, byte_we, byte_load, slt, lui, addm,
                    opcode, funct, zero);
     output [2:0] alu_op;
-    output [1:0] alu_src2;
+    output  alu_src2;
     output       writeenable, rd_src, except;
     output [1:0] control_type;
     output       mem_read, word_we, byte_we, byte_load, slt, lui, addm;
     input  [5:0] opcode, funct;
     input        zero;
+
 
 
     wire addi = (opcode == `OP_ADDI);
@@ -55,6 +108,8 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
     wire addm1 = (opcode == `OP_OTHER0) & (funct == `OP0_ADDM);
     wire slt1 = (opcode == `OP_OTHER0) & (funct == `OP0_SLT);
     wire jr1 = (opcode == `OP_OTHER0) & (funct == `OP0_JR);
+
+
     assign control_type[0] = ((bne1 & ~zero) | (beq1 & zero) | jr1);
     assign control_type[1] = (j1 | jr1);
 
@@ -66,4 +121,15 @@ module mips_decode(alu_op, writeenable, rd_src, alu_src2, except, control_type,
     assign alu_src2 = (lw1 | lbu1 | sw1 | sb1 | addi | andi | ori | xori);
     assign except = ~(nor1 | xor1 | addi | andi | lw1 | lbu1 | sw1 | sb1 | addm1 | add | sub | andd | or1 | ori | xori | bne1 | beq1 | j1 | jr1 | lui1 | slt1);
 
-    endmodule // mips_decode
+
+
+    assign lui = lui1;
+    assign slt = slt1;
+    assign word_we = sw1;
+    assign byte_we = sb1;
+    assign mem_read = (lw1 | lbu1 | addm1);
+    assign addm = addm1;
+    assign byte_load = lbu1;
+    //test
+
+endmodule // mips_decode
--- Lab5/full_machine.v
+++ Lab5/full_machine.v
@@ -4,25 +4,139 @@
 // clock   (input) - the clock signal
 // reset   (input) - set to 1 to set all registers to zero, set to 0 for normal execution.
 
+// module full_machine(except, clock, reset);
+//     output      except;
+//     input       clock, reset;
+
+//     wire [31:0] inst;  
+//     wire [31:0] PC;  
+
+//     // DO NOT comment out or rename this module
+//     // or the test bench will break
+//     register #(32) PC_reg( /* connect signals */);
+
+//     // DO NOT comment out or rename this module
+//     // or the test bench will break
+//     instruction_memory im( /* connect signals */ );
+
+//     // DO NOT comment out or rename this module
+//     // or the test bench will break
+//     regfile rf ( /* connect signal wires */);
+
+//     /* add other modules */
+
+// endmodule // full_machine
 module full_machine(except, clock, reset);
     output      except;
     input       clock, reset;
 
     wire [31:0] inst;  
-    wire [31:0] PC;  
+    wire [31:0] PC;
+    wire [31:0] trashslt, trashrmem, trashload, dataout, addmtrsh, luitrsh, ext, neg, load, jaddr, branchoff, addr, nextPC, rsData, rtData, rdData, PC4, PCbo, B, imm32, out;
+    wire weword, webyte, readmem, addm, overflow, zero, negative, rdsrc, wren, alusrc2, lui, slt, loadbyte;
+    wire [7:0] muxdatatrsh;
+    wire [1:0] control_type;
+    wire [2:0] alu_op;
+    wire [4:0] rdest;
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    register #(32) PC_reg( /* connect signals */);
+    register #(32) PC_reg(PC, nextPC, clock, 1'b1, reset);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    instruction_memory im( /* connect signals */ );
+    instruction_memory im(inst, PC[31:2]);
 
     // DO NOT comment out or rename this module
     // or the test bench will break
-    regfile rf ( /* connect signal wires */);
+    regfile rf (rsData, rtData, inst[25:21], inst[20:16], rdest, rdData, wren, clock, reset);
 
     /* add other modules */
 
-endmodule // full_machine
+    mips_decode code(alu_op, wren, rdsrc, alusrc2, except, control_type, readmem, weword, webyte, loadbyte, lui, slt, addm, inst[31:26], inst[5:0], zero);
+    data_mem mem(dataout, addr, rtData, weword, webyte, clock, reset);  
+
+    loadupper lu(load, inst[15:0]);
+    jraddress j(jaddr, inst[25:0], PC);
+    signextender sign_ext(imm32, inst[15:0]);
+    shiftleft sl(branchoff, imm32[29:0]);
+    byteextend be1(ext, muxdatatrsh);
+    negativeextend next1(neg, negative);
+
+
+
+
+
+    mux2v #(32) memRead(trashrmem, trashslt, trashload, readmem);
+    mux2v #(32) byteMux(trashload, dataout, ext, loadbyte);
+    mux2v #(32) Bmux(B, rtData, imm32, alusrc2);
+    mux2v #(32) luiMux(luitrsh, trashrmem, load, lui);
+    mux2v #(5) rdest_(rdest, inst[15:11], inst[20:16], rdsrc);
+    mux2v #(32) addmMux(rdData, luitrsh, addmtrsh,  addm);
+    mux2v #(32) ad(addr, out, rsData, addm);
+    mux2v #(32) sltMux(trashslt, out, neg, slt);
+    mux4v #(8) dataMux(muxdatatrsh, dataout[7:0], dataout[15:8], dataout[23:16], dataout[31:24], out[1:0]);
+    mux4v #(32) pcpicker(nextPC, PC4, PCbo, jaddr, rsData, control_type);
+
+    alu32 outAlu(out, overflow, zero, negative, rsData, B, alu_op);
+    alu32 addmALU(addmtrsh, , , , dataout, rtData, `ALU_ADD);
+    alu32 add4(PC4, , , , PC, 32'b100, `ALU_ADD);
+    alu32 br(PCbo, , , , PC4, branchoff, `ALU_ADD);
+
+
+    endmodule // full_machine
+
+module signextender(imm32, imm16);
+
+    output [31:0] imm32;
+    input [15:0] imm16;
+    assign imm32[15:0] = imm16[15:0];
+    assign imm32[31:16] = {16{imm16[15]}};
+endmodule // signextender
+
+module jraddress(out, address, pc);
+
+    output  [31:0] out;
+    input   [25:0] address;
+    input   [31:0] pc;
+    assign out[31:28] = pc[31:28];
+    assign out[27:2] = address[25:0];
+    assign out[1:0] = 2'b0;
+
+endmodule // signextender
+
+module byteextend(out, in);
+
+    output  [31:0] out;
+    input   [7:0] in;
+    assign out[31:8] = 24'b0;
+    assign out[7:0] = in[7:0];
+
+endmodule // signextender
+
+module negativeextend(out, in);
+
+    output  [31:0] out;
+    input   in;
+    assign out[31:1] = 31'b0;
+    assign out[0] = in;
+
+endmodule // signextender
+
+module loadupper(out, in);
+
+    output  [31:0] out;
+    input   [15:0] in;
+    assign out[31:16] = in[15:0];
+    assign out[15:0] = 16'b0;
+
+endmodule // signextender
+
+module shiftleft(out, in);
+
+    output [31:0] out;
+    input [29:0] in;
+    assign out[31:2] = in[29:0];
+    assign out[1:0] = 2'b0;
+
+endmodule // signextender
