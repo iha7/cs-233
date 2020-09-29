98be4c56dfed6889d874cfe064484cc8166e7b35
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-09-29 16:29:20 -0500

    things happened

--- Lab5/fm.s
+++ Lab5/fm.s
@@ -1,47 +1,29 @@
-# add your own tests for the full machine here!
-# feel free to take inspiration from all.s and/or lwbr.s
+
+    # add your own tests for the full machine here!
+# feel free to take inspiration from all.s and/or lwbr2.s
 
 .data
 # your test data goes here
+a: .word 0 1 2 4 8 16
+array:	.word	1	255	1024
 
 .text
-main:
-	# your test code goes here
+# main:
+# 	la	$4, a
+# 	lw	$5, 0($4)
+# 	lw	$6, 4($4)
+# 	lui $29, 14687
+main:   addi	$10, $0, 255		# $10 = 255
+	la	$2, array   		# $2  = 0x10010000
+	lw	$3, 0($2)		# $3  = 1
+	lw	$4, 4($2)		# $4  = 255
+	beq	$4, $10, equal		# branch taken
 
+	addi	$11, $0, 1		# skipped
+	j	end	    		# skipped
 
-la    $2, array    # $2  = 0x10010000    testing lui
+equal:	addi	$12, $0, 2		# $12 = 2
+	bne	$4, $10, end		# branch not taken
+	add	$14, $3, $4		# $14 = 256
 
-    lw    $3, 12($2)    # $3  = 0xcafebabe
-    slt    $8, $3, $0    # $8  = 1          testing slt
-    slt    $9, $2, $0    # $9  = 0
-    slt    $10, $0, $2    # $10 = 1
-    slt    $11, $2, $3    # $11 = 0
-
-    bne    $9, $0, end
-    beq    $11, $8, target
-
-    lbu    $4, 12($2)    # $4 = be        test load byte unsigned
-    lbu    $5, 13($2)    # $5 = ba
-    lbu    $6, 14($2)    # $6 = fe
-    lbu    $7, 15($2)    # $7 = ca
-
-    sw    $0, 0($2)    #            test stores
-    lw    $12, 0($2)    # $12 = 0
-    sb    $4, 2($2)
-    lw    $13, 0($2)    # $13 = 0x00be0000
-
-    addm    $18, $2, $2    # $18 = 0x10bf0000
-
-    lw    $14, 16($2)    # $14 = target = 0x0000005c
-    jr    $14          #  PC = 0x0000005c    test indirect branches
-
-skipped:
-    addi    $15, $zero, 1    # skipped so $15 remains 0
-    j    skipped            # skipped
-
-end:    lui    $17, 0xf00f    # $17 = 0xf00f0000
-    j    end
-
-target:
-    addi    $16, $zero, 2    # $16 = 2
-    bne    $16, $0, end    # unconditional jump
+end:	addi	$13, $0, 3		# $13 = 3
--- Lab5/full_machine.v
+++ Lab5/full_machine.v
@@ -1,5 +1,5 @@
 // full_machine: execute a series of MIPS instructions from an instruction cache
-//
+
 // except (output) - set to 1 when an unrecognized instruction is to be executed.
 // clock   (input) - the clock signal
 // reset   (input) - set to 1 to set all registers to zero, set to 0 for normal execution.
@@ -24,119 +24,94 @@
 //     regfile rf ( /* connect signal wires */);
 
 //     /* add other modules */
-
 // endmodule // full_machine
+
+
+
 module full_machine(except, clock, reset);
     output      except;
     input       clock, reset;
 
-    wire [31:0] inst;  
-    wire [31:0] PC;
-    wire [31:0] trashslt, trashrmem, trashload, dataout, addmtrsh, luitrsh, ext, neg, load, jaddr, branchoff, addr, nextPC, rsData, rtData, rdData, PC4, PCbo, B, imm32, out;
-    wire weword, webyte, readmem, addm, overflow, zero, negative, rdsrc, wren, alusrc2, lui, slt, loadbyte;
-    wire [7:0] muxdatatrsh;
-    wire [1:0] control_type;
+
+
+
+    wire [7:0] byte_mux_output;
+    wire [4:0] writeittothisAdress;
     wire [2:0] alu_op;
-    wire [4:0] rdest;
+    wire [1:0] alu_src2, controlNumberType;
+    wire lui, addm, mem_read, word_we,  zero, rd_src, writeenable, slt, overflow, negative, except, byte_we, byte_load;
+    wire [31:0] inst, PC, next_PC, rsData,  luiinput0, luiinput1, lui_mux_out, offsetThing, B, out, B_data, imm16, PC_control_0, PC_control_1, PC_control_2, PC_control_3;
+    wire [31:0] addmOutput, rdData, byte_extended, byteOutPut, slt_input_1, slt_output,  data_out, sign_extended;
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
-    register #(32) PC_reg(PC, nextPC, clock, 1'b1, reset);
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
+
+
+    // Decoder
+    mips_decode d(alu_op, writeenable, rd_src, alu_src2, except, controlNumberType,
+                   mem_read, word_we, byte_we, byte_load, slt, lui, addm,inst[31:26], inst[5:0], zero);
+
+
+    // Sign extender
+    assign sign_extended[15:0] = inst[15:0];
+    assign imm16[15:0] = inst[15:0];
+    assign sign_extended[31:16] = 16'b0;
+    assign imm16[31:16] = {16{inst[15]}};
+
+
+
+    // Control unit
+    alu32 a1(PC_control_0, , , , PC, 32'h4, 3'b010);
+    alu32 a2(PC_control_1, , , , offsetThing, PC_control_0, 3'b010);
+    register #(32) PC_reg(PC, next_PC, clock, 1'b1, reset);
     instruction_memory im(inst, PC[31:2]);
+    mux4v m1(next_PC, PC_control_0, PC_control_1, PC_control_2, rsData, controlNumberType);
+ 
 
-    // DO NOT comment out or rename this module
-    // or the test bench will break
-    regfile rf (rsData, rtData, inst[25:21], inst[20:16], rdest, rdData, wren, clock, reset);
+    assign luiinput1 = {inst[15:0], 16'b0};
+    assign slt_input_1 = {31'b0, negative};
+    assign slt_input_0 = out;
+    assign byte_extended = {24'b0, byte_mux_output};
 
-    /* add other modules */
-
-    mips_decode code(alu_op, wren, rdsrc, alusrc2, except, control_type, readmem, weword, webyte, loadbyte, lui, slt, addm, inst[31:26], inst[5:0], zero);
-    data_mem mem(dataout, addr, rtData, weword, webyte, clock, reset);  
-
-    loadupper lu(load, inst[15:0]);
-    jraddress j(jaddr, inst[25:0], PC);
-    signextender sign_ext(imm32, inst[15:0]);
-    shiftleft sl(branchoff, imm32[29:0]);
-    byteextend be1(ext, muxdatatrsh);
-    negativeextend next1(neg, negative);
+    assign PC_control_3 = rsData;
+    assign PC_control_2[31:28] = PC_control_0[31:28];
+    assign PC_control_2[27:2] = inst[25:0];
+    assign PC_control_2[1:0] = 2'b0;
 
 
+        // Data Memory
+    data_mem dm(data_out, out, B_data, word_we, byte_we, clock, reset);
+    mux4v m5(byte_mux_output, data_out[7:0], data_out[15:8], data_out[23:16], data_out[31:24], out[1:0]);
+    mux2v m6(luiinput0, slt_output, byteOutPut, mem_read);
+    mux2v m7(byteOutPut, data_out[31:0], byte_extended, byte_load);
+    mux2v m8(slt_output, out, slt_input_1, slt);
+
+    
+    // Regfile and alu
+    mux2v m3(lui_mux_out, luiinput0, luiinput1, lui);
+    mux3v m4(B, B_data, imm16, sign_extended, alu_src2);
+    alu32 a3(out, overflow, zero, negative, rsData, B, alu_op);
+    mux2v m2(writeittothisAdress, inst[15:11], inst[20:16], rd_src);
+    regfile rf (rsData, B_data, inst[25:21], inst[20:16], writeittothisAdress, rdData, writeenable, clock, reset);
 
 
 
-    mux2v #(32) memRead(trashrmem, trashslt, trashload, readmem);
-    mux2v #(32) byteMux(trashload, dataout, ext, loadbyte);
-    mux2v #(32) Bmux(B, rtData, imm32, alusrc2);
-    mux2v #(32) luiMux(luitrsh, trashrmem, load, lui);
-    mux2v #(5) rdest_(rdest, inst[15:11], inst[20:16], rdsrc);
-    mux2v #(32) addmMux(rdData, luitrsh, addmtrsh,  addm);
-    mux2v #(32) ad(addr, out, rsData, addm);
-    mux2v #(32) sltMux(trashslt, out, neg, slt);
-    mux4v #(8) dataMux(muxdatatrsh, dataout[7:0], dataout[15:8], dataout[23:16], dataout[31:24], out[1:0]);
-    mux4v #(32) pcpicker(nextPC, PC4, PCbo, jaddr, rsData, control_type);
-
-    alu32 outAlu(out, overflow, zero, negative, rsData, B, alu_op);
-    alu32 addmALU(addmtrsh, , , , dataout, rtData, `ALU_ADD);
-    alu32 add4(PC4, , , , PC, 32'b100, `ALU_ADD);
-    alu32 br(PCbo, , , , PC4, branchoff, `ALU_ADD);
 
 
-    endmodule // full_machine
+    // Addm implementation - not working
+    mux2v m9(rdData, lui_mux_out, addmOutput, addm);
+    alu32 a4(addmOutput, , , , data_out, Binput, 3'b010);
 
-module signextender(imm32, imm16);
 
-    output [31:0] imm32;
-    input [15:0] imm16;
-    assign imm32[15:0] = imm16[15:0];
-    assign imm32[31:16] = {16{imm16[15]}};
-endmodule // signextender
+    // Bit shift
+    bitshift b1(offsetThing, imm16[29:0]);   
 
-module jraddress(out, address, pc);
+    
+endmodule // full_machine
 
-    output  [31:0] out;
-    input   [25:0] address;
-    input   [31:0] pc;
-    assign out[31:28] = pc[31:28];
-    assign out[27:2] = address[25:0];
-    assign out[1:0] = 2'b0;
+module bitshift(offsetThing, imm16);
+   output [31:0] offsetThing;
+   input [29:0] imm16;
+   assign offsetThing[31:2] = imm16[29:0];
+   assign offsetThing[1:0] = 0;
 
-endmodule // signextender
-
-module byteextend(out, in);
-
-    output  [31:0] out;
-    input   [7:0] in;
-    assign out[31:8] = 24'b0;
-    assign out[7:0] = in[7:0];
-
-endmodule // signextender
-
-module negativeextend(out, in);
-
-    output  [31:0] out;
-    input   in;
-    assign out[31:1] = 31'b0;
-    assign out[0] = in;
-
-endmodule // signextender
-
-module loadupper(out, in);
-
-    output  [31:0] out;
-    input   [15:0] in;
-    assign out[31:16] = in[15:0];
-    assign out[15:0] = 16'b0;
-
-endmodule // signextender
-
-module shiftleft(out, in);
-
-    output [31:0] out;
-    input [29:0] in;
-    assign out[31:2] = in[29:0];
-    assign out[1:0] = 2'b0;
-
-endmodule // signextender
+endmodule // shift_left_two
