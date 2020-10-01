98fcad6ecbfa437c3b44aac0f8176aeb41f63ccd
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-10-01 17:27:48 -0500

    pt1

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -10,6 +10,52 @@
 #     }
 #   }
 # }
+
+
 .globl fill_array
 fill_array:
-    jr      $ra
+
+    li      $t0, 0              
+
+    move    $t1, $a1           
+
+loop:
+    bge     $t0, 6, end        
+
+    srl     $a0, $a0, 3       
+
+    li      $t2, 3          
+
+    div     $t3, $t0, $t2   
+
+    mfhi    $t3             
+
+    bne     $t3, $0, else      
+
+    and     $t4, $a0, 31       
+
+    mul     $t3, $t0, 4        
+
+    add     $t5, $t1, $t3    
+
+    sw     $t4, 0($t5)     
+
+    addi    $t0, $t0, 1        
+
+    j loop
+
+else:
+    mul    $t3, $t0, 4         
+
+    add    $t5, $t1, $t3       
+
+    and    $t4, $a0, 255      
+
+    sw     $t4, 0($t5)       
+
+    add    $t0, 1      
+          
+    j loop
+
+end:
+    jr    $r
