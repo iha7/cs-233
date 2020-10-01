b44f5d7592664618c28fd9a50c9d1108d35d1aa7
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-10-01 18:10:50 -0500

    fixed things

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -10,52 +10,49 @@
 #     }
 #   }
 # }
-
-
 .globl fill_array
 fill_array:
 
-    li      $t0, 0              
+    li      $t0, 0      
 
-    move    $t1, $a1           
+    move    $t1, $a1          
 
 loop:
-    bge     $t0, 6, end        
+    bge     $t0, 6, exit        
 
-    srl     $a0, $a0, 3       
+    srl     $a0, $a0, 3        
 
-    li      $t2, 3          
+    li      $t2, 3            
 
-    div     $t3, $t0, $t2   
+    div     $t3, $t0, $t2      
 
-    mfhi    $t3             
+    mfhi    $t3          
 
-    bne     $t3, $0, else      
+    bne     $t3, $0, else       
 
-    and     $t4, $a0, 31       
+    and     $t4, $a0, 31      
 
-    mul     $t3, $t0, 4        
+    mul     $t3, $t0, 4      
 
-    add     $t5, $t1, $t3    
+    add     $t5, $t1, $t3      
 
-    sw     $t4, 0($t5)     
+    sw     $t4, 0($t5)       
 
-    addi    $t0, $t0, 1        
+    addi    $t0, $t0, 1      
 
     j loop
 
 else:
-    mul    $t3, $t0, 4         
+    mul    $t3, $t0, 4          
 
-    add    $t5, $t1, $t3       
+    add    $t5, $t1, $t3        
 
     and    $t4, $a0, 255      
+    sw     $t4, 0($t5)         
 
-    sw     $t4, 0($t5)       
+    add    $t0, 1           
 
-    add    $t0, 1      
-          
     j loop
 
-end:
-    jr    $r
+exit:
+    jr    $ra
