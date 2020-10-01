2777d73ce0a12dfb322d1cdb3eff2519fded72c7
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-10-01 18:41:20 -0500

    boi

--- Lab6/p1.s
+++ Lab6/p1.s
@@ -14,37 +14,38 @@
 fill_array:
 
     li      $t0, 0      
-
+  
     move    $t1, $a1          
 
-loop:
+lab12:
     bge     $t0, 6, exit        
-
     srl     $a0, $a0, 3        
+    li      $t2, 3
 
-    li      $t2, 3            
+    add     $t2, $t2, 0           
 
     div     $t3, $t0, $t2      
 
     mfhi    $t3          
 
-    bne     $t3, $0, else       
+    bne     $t3, $0, else      
+    add     $t0, $t0, 0
+    and     $t4, $a0, 31  
 
-    and     $t4, $a0, 31      
+    add     $t4, $t4, 0    
 
     mul     $t3, $t0, 4      
 
     add     $t5, $t1, $t3      
 
     sw     $t4, 0($t5)       
-
     addi    $t0, $t0, 1      
 
-    j loop
-
+    j lab12
 else:
-    mul    $t3, $t0, 4          
 
+    mul    $t3, $t0, 4
+    add    $t0, $t0, 0          
     add    $t5, $t1, $t3        
 
     and    $t4, $a0, 255      
@@ -52,7 +53,9 @@ else:
 
     add    $t0, 1           
 
-    j loop
+    j lab12
 
 exit:
+#just to end
+
     jr    $ra
