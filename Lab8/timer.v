697f46546a7ec66687fd2f4192bcafb33c97283b
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-10-15 17:10:19 -0500

    part1

--- Lab8/timer.v
+++ Lab8/timer.v
@@ -11,19 +11,6 @@ module timer(TimerInterrupt, cycle, TimerAddress,
     // HINT: make your interrupt cycle register reset to 32'hffffffff
     //       (using the reset_value parameter)
     //       to prevent an interrupt being raised the very first cycle
-module timer(TimerInterrupt, cycle, TimerAddress,
-             data, address, MemRead, MemWrite, clock, reset);
-    output        TimerInterrupt;
-    output [31:0] cycle;
-    output        TimerAddress;
-    input  [31:0] data, address;
-    input         MemRead, MemWrite, clock, reset;
-
-    // complete the timer circuit here
-
-    // HINT: make your interrupt cycle register reset to 32'hffffffff
-    //       (using the reset_value parameter)
-    //       to prevent an interrupt being raised the very first cycle
 
     wire    [31:0] Q, D, Q2;
     wire    Acknowledge, cE6, cE1, TimerWrite, TimerRead, resettwo, Qequality;
