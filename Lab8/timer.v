ef3675240355518bb314e5b8cbf5f93d44f8726d
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-10-15 17:22:28 -0500

    part 1 again pls work lol

--- Lab8/timer.v
+++ Lab8/timer.v
@@ -12,25 +12,25 @@ module timer(TimerInterrupt, cycle, TimerAddress,
     //       (using the reset_value parameter)
     //       to prevent an interrupt being raised the very first cycle
 
-    wire    [31:0] Q, D, Q2;
-    wire    Acknowledge, cE6, cE1, TimerWrite, TimerRead, resettwo, Qequality;
+    wire    [31:0] a, b, a2;
+    wire    Acknowledge, cE6, cE1, write_time, TimerRead, resettwo, equal;
 
-    register count(Q, D, clock, 1,reset);
-    alu32 add_alu(D, , ,`ALU_ADD, Q, 32'h1);
-    
+    register count(a, b, clock, 1,reset);
+    alu32 add_alu(b, , ,`ALU_ADD, a, 32'h1);
     register #(32, 32'hffffffff)
-    cycle_intr(Q2, data, clock, TimerWrite, reset);
+    cycle_intr(a2, data, clock, write_time, reset);
 
-    assign Qequality = (Q == Q2);
-    tristate #(32) three_read(cycle, Q, TimerRead);
+    assign equal = (a == a2);
+    tristate #(32) three_read(cycle, a, TimerRead);
 
     assign resettwo = (Acknowledge | reset);
-    dffe intr(TimerInterrupt, 1, clock, Qequality, resettwo);
+
+    dffe intr(TimerInterrupt, 1, clock, equal, resettwo);
 
     assign TimerAddress = (cE1 | cE6);
     assign Acknowledge = (cE6 & MemWrite);
     assign TimerRead = (cE1 & MemRead);
-    assign TimerWrite = (cE1 & MemWrite);
+    assign write_time = (cE1 & MemWrite);
 
     assign cE1 = ('hffff001c == address);
     assign cE6 = ('hffff006c == address);
