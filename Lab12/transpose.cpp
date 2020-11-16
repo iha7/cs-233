8c4db8106529df6b4bc3705d95019d64811524e8
Isaac Ha <isaacha2@linux-a2.ews.illinois.edu>
2020-11-16 00:38:52 -0600

    part 1

--- Lab12/transpose.cpp
+++ Lab12/transpose.cpp
@@ -1,11 +1,28 @@
+// #include "transpose.h"
+// int min(int, int);
+
+// // modify this function to add tiling
+// void transpose_tiled(int **A, int **B) {
+//     for (int i = 0; i < SIZE; i ++) {
+//         for (int j = 0; j < SIZE; j ++) {
+//             B[i][j] = A[j][i];
+//         }
+//     }
+// }
+
+
+
 #include "transpose.h"
 int min(int, int);
-
-// modify this function to add tiling
 void transpose_tiled(int **A, int **B) {
-    for (int i = 0; i < SIZE; i ++) {
-        for (int j = 0; j < SIZE; j ++) {
-            B[i][j] = A[j][i];
+    int newSize = 32;
+    for (int i = 0; i < SIZE; i+= newSize) {
+        for (int j = 0; j < SIZE; j=+ newSize) {
+            for (int k = i; k < min(i + newSize, SIZE); k++) {
+                for (int l = j; l < min (j + newSize, SIZE); l++) {
+                    A[k][l] = B[l][k];
+                }
+            }
         }
     }
 }
